a481cfd6e6aec7e5da455253af882e04a0361b55
John Holstein <jrhwork@me.com>
2015-03-08 18:33:54 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,11 @@
+#project 1 Plot 1 Data gather and histogram
+#Exploratory Data Analysis Class
+setwd("~/Coursera")
+dataFile <- "~household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# pull only data I want
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
