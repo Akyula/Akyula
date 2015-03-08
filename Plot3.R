98d66258a10ea188324121daf40b1929dcb887d7
John Holstein <jrhwork@me.com>
2015-03-08 18:53:01 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,7 +1,7 @@
 #project 1 Plot 3 Data gather and histogram
 #Exploratory Data Analysis Class
 setwd("~/Coursera")
-dataFile <- "./data/household_power_consumption.txt"
+dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
